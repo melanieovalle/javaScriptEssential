162e174c6bad979185c8e523b00508b5773d7447
Melanie Ovalle <melanieovalle008@gmail.com>
2025-05-22 14:53:27 -0400

    control flow practice

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -24,9 +24,27 @@ function show(){
     var functionVar = "I'm a block-scoped var";
     let functionLet = "I'm a block-scoped let";
     const functionConst = "I'm a block-scoped const";
-    }
-    show();
-    
-    console.log(functionVar); // Throws ReferenceError
-    console.log(functionLet); // Throws ReferenceError
-    console.log(functionConst); // Throws ReferenceError
+}
+show();
+
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
+
+{
+    let var1 = "let hello";
+    const var2 = "const hello";
+    var var3 = "var hello";
+
+    var1 = "new hello";
+    //var2 = "new hello";
+    var3 = "new hello";
+}
+
+var1 = "let new hello 2.0";
+//var2 = "new hello 2.0";
+var3 = "var new hello 2.0";
+
+console.log(var1);
+//console.log(var2);
+console.log(var3);
